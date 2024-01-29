312315657c6fea1178b916cf152987e2abce6f75
liling217 <liling217>
2024-01-29 22:35:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer.", "YouTuber.", "Web Developer."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+function redirWatsapp() {
+  window.location.href = "https://wa.me/+6283896705342";
+}
